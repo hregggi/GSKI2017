98fe31344b1445c577e4caec82d381e34adcc5db
hregggi <hreggvidure@gmail.com>
2017-01-29 13:30:11 +0000

    Setti upp stringlistcpp

--- Verkefni2/Stringlist/StringList.cpp
+++ Verkefni2/Stringlist/StringList.cpp
@@ -1,56 +1,110 @@
 #include "StringList.h"
 
-    StringList();
-    ~StringList();
+StringList::StringList()
+{
+
+}
+
+StringList::~StringList()
+{
+
+}
 
     // Clear contents from the list, to make it empty.
     // Worst-case time complexity: Linear
-    void clear();
+void StringList::clear()
+{
+
+}
+
     // Insert an element at the current location.
     // item: The element to be inserted
     // Worst-case time complexity: Constant
-    void insert(const string& item);
+void StringList::insert(const string& item)
+{
+
+}
+
     // Append an element at the end of the list.
     // item: The element to be appended.
     // Worst-case time complexity: Constant
-    void append(const string& item);
+void StringList::append(const string& item)
+{
+
+}
+
     // Remove and return the current element.
     // Return: the element that was removed.
     // Worst-case time complexity: Constant
     // Throws InvalidPositionException if current position is
     // behind the last element
-    string remove();
+string StringList::remove()
+{
+
+}
+
     // Set the current position to the start of the list
     // Worst-case time complexity: Constant
-    void move_to_start();
+void StringList::move_to_start()
+{
+
+}
+
     // Set the current position to the end of the list
     // Worst-case time complexity: Constant
-    void move_to_end();
+void StringList::move_to_end()
+{
+
+}
 
     // Move the current position one step left. No change
     // if already at beginning.
     // Worst-case time complexity: Constant
-    void prev();
+void StringList::prev()
+{
+
+}
+
     // Move the current position one step right. No change
     // if already at end.
     // Worst-case time complexity: Constant
-    void next();
+void StringList::next()
+{
+
+}
+
     // Return: The number of elements in the list.
     // Worst-case time complexity: Constant
-    int length() const;
+int StringList::length() const
+{
+
+}
+
     // Return: The position of the current element.
     // Worst-case time complexity: Constant
-    int curr_pos() const;
+int StringList::curr_pos() const
+{
+
+}
+
     // Set current position.
     // pos: The position to make current.
     // Worst-case time complexity: Linear
     // Throws InvalidPositionException if 'pos' is not a valid position
-    void move_to_pos(int pos);
+void StringList::move_to_pos(int pos)
+{
+
+}
+
     // Return: The current element.
     // Worst-case time complexity: Constant
     // Throws InvalidPositionException if current position is
     // behind the last element
-    const string& get_value() const;
+const string& StringList::get_value() const
+{
+
+}
+
     // Outputs the elements of 'lis' to the stream 'outs', separated
     // by a single space.
-    friend ostream& operator <<(ostream& outs, const StringList& lis);
+friend ostream& operator <<(ostream& outs, const StringList& lis);
