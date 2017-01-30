1c90bc132971f2a2a73c9ae7841ba1e17904fa3d
hregggi <hreggvidure@gmail.com>
2017-01-30 21:51:56 +0000

    kláraði
    v

--- Verkefni2/Stringlist/StringList.cpp
+++ Verkefni2/Stringlist/StringList.cpp
@@ -4,32 +4,30 @@ using namespace std;
 
 StringList::StringList()
 {
-    head = new StringNode("H",NULL,NULL);    //head bendir hvergi
-    tail = new StringNode("T",NULL,head);    //tail bendir hvergi
-    head->prev = tail;
-    curr = tail;
-    sise = 0;
+    head = new StringNode("H", NULL, NULL);     //head bendir hvergi
+    tail = new StringNode("T", NULL, head);     //tail bendir á head
+    head->next = tail;                          // head bnir á tail
+    curr = tail;                                // curr bendir á tail
+    sise = 0;                                   // stærð er 0 og staðsetning er 0
     cPos = 0;
 }
 
 StringList::~StringList()
 {
-    StringNode *node = head;        // bý til tímabundna breytu
-    while(node != NULL)             // meðan hún er ekki komin á enda
-    {
-        head = node;                // fer í gegnum listan frá head til tail
-        node = node->next;          // og eyði hverju staki fyrir sig.
-        delete head;
-    }
-    head = NULL;
-    tail = NULL;
+    clear();                                    // eyði innihaldi listans
+    delete head;                                // eyði haus listans
+    delete tail;                                // eyði tail istans
 }
 
     // Clear contents from the list, to make it empty.
     // Worst-case time complexity: Linear
 void StringList::clear()
 {
-
+    move_to_start();                            //  set curr fremst
+    while(sise != 0)                            // meðan eithvað er í lisatanum
+    {
+        remove();                               // er hreinsað næsta stak úr listanum
+    }
 }
 
     // Insert an element at the current location.
@@ -38,10 +36,11 @@ void StringList::clear()
 void StringList::insert(const string& item)
 {
     StringNode *node = new StringNode(item, curr, curr->prev);
-    curr->prev->next = node;
-    curr->prev = node;
-    sise++;
-    cPos++;
+                                                // nýtt node komið fyrir framan curr
+    curr->prev->next = node;                    // stakið sem var fyrir framan curr bendir nú á nýja
+    curr->prev = node;                          // stakið sem curr var að benda á bendir nú á nýja
+    curr = node;                                // curr bendir nú á nýja
+    sise++;                                     // listi hefur stækkað um eitt stak
 }
 
     // Append an element at the end of the list.
@@ -49,12 +48,16 @@ void StringList::insert(const string& item)
     // Worst-case time complexity: Constant
 void StringList::append(const string& item)
 {
-    StringNode *node = new StringNode(item, tail, NULL);
+    StringNode *node = new StringNode(item, tail, tail->prev);
+                                                // nýja stakið er fyrir framan tail
+    tail->prev->next = node;                    // það sem var fyrir framan tail bendir nú á nýja
+    tail->prev = node;                          // og tail bendir líka á nýja
+    sise++;                                     // fylki hefur stækkað um 1
 
-    tail->prev->next = node;
-    node->prev = tail->prev;
-    tail->prev = node;
-    sise++;
+    if(curr == tail)                            // ef curr var að benda á tail
+    {
+        curr = tail->prev;                      // þá bendir það nú á nýja stakið
+    }
 }
 
     // Remove and return the current element.
@@ -64,29 +67,23 @@ void StringList::append(const string& item)
     // behind the last element
 string StringList::remove()
 {
-    if(curr == tail)
+    if(cPos >= sise || sise == 0)               // ef fylkið er 0 stórt eða curr er komið á tail
     {
-        throw InvalidPositionException();
-    }
-    else if (cPos <= 0)
-    {
-
+        throw InvalidPositionException();       // er kastað villu um ólöglega stöðu
     }
     else
     {
-    string text = curr->data;
-    StringNode *ptr = curr->prev;
+        string text = curr->data;               // breyta geymir textan úr stakinu sem skal fjarlægja
+        StringNode *ptr = curr;                 // nýr bendir bendir á sama og ccurr bendirinn bendir á..... bendir
 
-    curr->prev->next = curr->next;
-    curr->next->prev = curr->prev;
-    delete &curr;
+        ptr->prev->next = ptr->next;            // nóðurnar sitthvoru megin við stakið sem skal eyða
+        ptr->next->prev = ptr->prev;            // benda nú á hvor aðra.
 
-    curr = ptr;
+        curr = curr->next;                      // curr fær gyldið á nóðunni sem var fyrir framan hana
 
-    sise--;
-    cPos--;
-
-    return text;
+        delete ptr;                             // eyði út gyldinu sme átti að eyða
+        sise--;                                 // listinn hefur nú minnkað í stærð
+        return text;                            // og textanum skilað til main, eða hvert sem hann átti að fara
     }
     return "";
 }
@@ -95,16 +92,16 @@ string StringList::remove()
     // Worst-case time complexity: Constant
 void StringList::move_to_start()
 {
-    curr = head;
-    cPos = 0;
+    cPos = 0;                                   // staðsetning curr er núna fremst
+    curr = head->next;                          // þeas beint fyrir framan head
 }
 
     // Set the current position to the end of the list
     // Worst-case time complexity: Constant
 void StringList::move_to_end()
 {
-    curr = tail;
-    cPos = sise;
+    cPos = sise;                                // staðsetning curr er núna aftast
+    curr = tail;                                // þeas á tail
 }
 
     // Move the current position one step left. No change
@@ -112,10 +109,10 @@ void StringList::move_to_end()
     // Worst-case time complexity: Constant
 void StringList::prev()
 {
-    if(curr != head)
-    {                               // ef curr kemst framar
-        curr = curr->prev;          // er curr fært framar
-        cPos--;                     // og staðsetning uppfærð
+    if(cPos > 0)
+    {                                           // ef curr kemst framar
+        curr = curr->prev;                      // er curr fært framar
+        cPos--;                                 // og staðsetning uppfærð
     }
 }
 
@@ -124,10 +121,11 @@ void StringList::prev()
     // Worst-case time complexity: Constant
 void StringList::next()
 {
-    if(curr != tail)                // ef curr kemst aftar
-    {                               // er curr fært aftar
-        curr = curr->next;          // og staðsetning uppfærð
+    if(cPos < sise)                             // ef curr kemst aftar
+    {                                           // er curr fært aftar
+        curr = curr->next;                      // og staðsetning uppfærð
         cPos++;
+
     }
 }
 
@@ -135,14 +133,14 @@ void StringList::next()
     // Worst-case time complexity: Constant
 int StringList::length() const
 {
-    return sise;
+    return sise;               // skilar fjölda hlekkja í listanum (að head og tail utanskyldum)
 }
 
     // Return: The position of the current element.
     // Worst-case time complexity: Constant
 int StringList::curr_pos() const
 {
-    return cPos;
+    return cPos;                                // skila staðsetningu á curr bendinum
 }
 
     // Set current position.
@@ -151,13 +149,20 @@ int StringList::curr_pos() const
     // Throws InvalidPositionException if 'pos' is not a valid position
 void StringList::move_to_pos(int pos)
 {
-    while(pos > cPos)           // ef pos er ofar en staðsetning curr
-    {                           // er færrt curr upp þangað til þeir
-        next();                 // eru orðnir jafnir
+    if(pos > sise || pos < 0)
+    {                                           // ef pos er utan listans
+        throw InvalidPositionException();       // er kastað villu um ólöglega staðsetningu
     }
-    while(pos < cPos)           // ef pos er neðar en staðsetning curr
-    {                           // er fært curr niðurþangað til þeir
-        prev();                 // eru orðnir jafnir
+    else
+    {
+        while(pos > cPos)                       // ef pos er ofar en staðsetning curr
+        {                                       // er færrt curr upp þangað til þeir
+            next();                             // eru orðnir jafnir
+        }
+        while(pos < cPos)                       // ef pos er neðar en staðsetning curr
+        {                                       // er fært curr niðurþangað til þeir
+            prev();                             // eru orðnir jafnir
+        }
     }
 }
 
@@ -168,11 +173,11 @@ void StringList::move_to_pos(int pos)
 const string& StringList::get_value() const
 {
     if(curr == tail)
-    {
-        throw InvalidPositionException();
-        return NULL;
+    {                                           // ef curr er í tail
+        throw InvalidPositionException();       // er kastað villu um ólöglega staðsetningu
+        return NULL;                            // og engu skilað
     }
-    return curr->data;
+    return curr->data;                          // annars er skilað gögnunum úr nóðunni curr bendir á
 
 }
 
@@ -180,14 +185,15 @@ const string& StringList::get_value() const
     // by a single space.
 ostream& operator <<(ostream& outs, const StringList& lis)
 {
-    StringNode *ptr;
-    ptr = lis.head;
+    if(lis.sise > 0){                           // ef eithvað er á annaðborð í listanum
+        StringNode *ptr;                        // er núr bendir sentur í haus listans
+        ptr = lis.head;
 
-    while(ptr->next->next != NULL)
-    {
-        ptr = ptr->next;
-        outs << ptr->data << " ";
+        while(ptr->next->next != NULL)          // á meðan hann er tveimur frá tail
+        {
+            ptr = ptr->next;                    // er hann færður einn áfram
+            outs << ptr->data << " ";           // og svo prentað út stakið úr honum með bili á eftir
+        }
     }
-
-    return outs;
+    return outs;                                // cout á þá að geta prentað það út
 }
--- Verkefni2/Stringlist/main.cpp
+++ Verkefni2/Stringlist/main.cpp
@@ -5,59 +5,86 @@ using namespace std;
 
 int main()
 {
-    StringList listi;
 
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
+    cout << "creating list" << endl;
+        StringList listi;
 
-    listi.append("first");
-
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
-
-    listi.append("second");
-
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
-
-    listi.append("third");
-
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
+        listi.clear();
+        listi.insert("stuff");
 
 
-    listi.insert("fourth");
 
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
 
-    listi.insert("fifth");
 
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
 
-    listi.insert("sixth");
+    cout << "inserting first | " << listi.curr_pos() << " " << listi.length() <<endl;
+        listi.insert("first");
+    cout << listi << endl;
+    cout << "inserting second | " << listi.curr_pos() << " " << listi.length() <<endl;
+        listi.insert("second");
+    cout << listi << endl;
+    cout << "calling next five times | " << listi.curr_pos() << " " << listi.length() <<endl;
+        listi.next();
+        listi.next();
+        listi.next();
+        listi.next();
+        listi.next();
 
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
-
+    string what = "what";
+    for(int i = 0; i < 10; i++)
+    {
+        listi.append(what);
+    }
+    cout << listi << endl;
+    cout << "hreinsa lista | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.clear();
+    cout << "prenta tomann lista | "<< listi.curr_pos() << " " << listi.length()<< endl;
+    cout << listi << endl;
+    listi.insert("ins1");
+    listi.append("app1");
+    cout << listi << endl;
+    listi.insert("ins2");
+    listi.append("app2");
     cout << listi << endl;
 
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
+    listi.clear();
+    listi.clear();
+    cout << "prenta toman lista | " << listi.curr_pos() << " " << listi.length() << endl;
+    cout << listi;
+    cout << "baeti fullt af stokum | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.insert("app1");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.append("app2");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.append("app3");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.append("app4");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.insert("ins1");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.insert("ins2");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.insert("ins3");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
+    listi.insert("ins4");
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
 
-  //  listi.next();
+    cout << listi << " | " << listi.curr_pos() << " " << listi.length() << endl;
 
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
 
-    //listi.next();
+    listi.remove();
+    listi.remove();
+    listi.remove();
+    listi.remove();
 
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
-
-    //listi.prev();
-
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
-
-    cout << listi.remove() << endl;
-
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
-
-    cout << listi << endl;
-
-        cout<< "pos >" << listi.curr_pos() << " .len >" << listi.length() << endl;
-
+    StringList thessigaur;
+    cout << thessigaur << " bjo hann til | " << thessigaur.curr_pos() << " " << thessigaur.length() << endl;
+    thessigaur.move_to_start();
+    cout << thessigaur << " move to start | " << thessigaur.curr_pos() << " " << thessigaur.length() << endl;
+    thessigaur.clear();
+    cout << thessigaur << " cleared | " << thessigaur.curr_pos() << " " << thessigaur.length() << endl;
+    thessigaur.insert("lol");
+    cout << thessigaur << " inserted | " << thessigaur.curr_pos() << " " << thessigaur.length() << endl;
 
 
     return 0;
